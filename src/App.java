231c9df6a63880bce710d8872f524af14c5655e7
Marina-Mustafina <marina.vect.7@mail.ru>
2023-10-24 12:43:33 +0400

    Добавление окон для ввода данных

--- src/App.java
+++ src/App.java
@@ -74,6 +74,71 @@ public static void main(String[] args) {
     frame.add(button3);
     frame.add(button4);
     frame.add(button5);
+ // создание текстового окна
+    JTextField text1 = new JTextField();
+    text1.setSize(150,100); // размеры текста 
+    text1.setLocation(43,45); // расположение текста
+    text1.setBackground(color); // цвет фона
+    text1.setHorizontalAlignment(0); // текст по середине 
+    frame.add(text1); // отображение текста
+    text1.setText(""); // текст
+    text1.setFont(Font2); // шрифт и размер текста
+    text1.setForeground(Color.BLACK); // цвет текста 
+    
+    JTextField text2 = new JTextField();
+    text2.setSize(150,100);
+    text2.setLocation(260,45);
+    text2.setBackground(color);
+    text2.setHorizontalAlignment(0);
+    frame.add(text2);
+    text2.setText("");
+    text2.setFont(Font2);
+
+    JTextField text4 = new JTextField();
+    text4.setSize(150,100);
+    text4.setLocation(690,45);
+    text4.setBackground(color);
+    frame.add(text4);
+    text4.setHorizontalAlignment(0);
+    text4.setText("");
+    text4.setFont(Font2);
+
+    JTextField text5 = new JTextField();
+    text5.setSize(793,50);
+    text5.setLocation(43,330);
+    text5.setBackground(color); // цвет фона 
+    frame.add(text5);
+    text5.setHorizontalAlignment(0);
+    text5.setText(" Р е з у л ь т а т");
+    text5.setFont(Font4);
+    text5.setForeground(Color.BLACK); // цвет текста
+
+    JTextField text6 = new JTextField();
+    text6.setSize(150,80);
+    text6.setLocation(44,400);
+    text6.setBackground(Color.WHITE);
+    text6.setHorizontalAlignment(0);
+    frame.add(text6);
+    text6.setText("");
+    text6.setFont(Font2);
+
+    JTextField text7 = new JTextField();
+    text7.setSize(150,80);
+    text7.setLocation(343,400);
+    text7.setBackground(Color.WHITE);
+    text7.setHorizontalAlignment(0);
+    frame.add(text7);
+    text7.setText("");
+    text7.setFont(Font2);
+
+    JTextField text8 = new JTextField();
+    text8.setSize(150,80);
+    text8.setLocation(638,400);
+    text8.setBackground(Color.WHITE);
+    text8.setHorizontalAlignment(0);
+    frame.add(text8);
+    text8.setText("");
+    text8.setFont(Font2);
 
 }
 }
